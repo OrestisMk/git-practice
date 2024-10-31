1fcbb39457146a132b6ef95d3d34bd8693643fc6
orestis makris <orestismk.tuc@gmail.com~>
2024-10-31 14:18:34 +0100

    kkkkk

--- .github/actions/prose-lint/entrypoint.sh
+++ .github/actions/prose-lint/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
+set -o pipefail
 Linter=${INPUT_LINTER:-proselint}
+
+exit_code=0
 
 find . -type f -name "*.md" | while IFS= read -r file; do
 	echo "Linting $file with $LINTER"
-	if ["$LINTER" = "proselint" ]; then
-		proselint "$file" || exit_code=$?
+	if [ "$LINTER" = "proselint" ]; then
+		proselint "$file" || exit_code=1
 	else
-		write-good "$file" || exit_code=$?
+		write-good "$file" || exit_code=1
 	fi
 done
 
-exit ${exit_code:-0}
+exit $exit_code
