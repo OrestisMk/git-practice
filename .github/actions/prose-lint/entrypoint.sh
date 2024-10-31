f66762caf90983d8de6170d2f4d67e46452a2158
orestis makris <orestismk.tuc@gmail.com~>
2024-10-31 14:20:57 +0100

    kkkkk2

--- .github/actions/prose-lint/entrypoint.sh
+++ .github/actions/prose-lint/entrypoint.sh
@@ -6,8 +6,8 @@ Linter=${INPUT_LINTER:-proselint}
 exit_code=0
 
 find . -type f -name "*.md" | while IFS= read -r file; do
-	echo "Linting $file with $LINTER"
-	if [ "$LINTER" = "proselint" ]; then
+	echo "Linting $file with $Linter"
+	if [ "$Linter" = "proselint" ]; then
 		proselint "$file" || exit_code=1
 	else
 		write-good "$file" || exit_code=1
