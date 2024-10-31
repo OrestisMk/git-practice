bd019e62334845be93cc86bc73c738a7b8562525
orestis makris <orestismk.tuc@gmail.com~>
2024-10-31 14:14:35 +0100

    fix entrypoint

--- .github/actions/prose-lint/entrypoint.sh
+++ .github/actions/prose-lint/entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 set -e
+Linter=${INPUT_LINTER:-proselint}
 
-# Get the chosen linter (proselint or write-good)
-LINTER=${INPUT_LINTER:-proselint}
-
-# Find all .md files and run the selected linter on each
-for file in $(find . -type f -name "*.md"); do
-  echo "Linting $file with $LINTER"
-  if [ "$LINTER" = "proselint" ]; then
-    proselint "$file" || exit_code=$?
-  else
-    write-good "$file" || exit_code=$?
-  fi
+find . -type f -name "*.md" | while IFS= read -r file; do
+	echo "Linting $file with $LINTER"
+	if ["$LINTER" = "proselint" ]; then
+		proselint "$file" || exit_code=$?
+	else
+		write-good "$file" || exit_code=$?
+	fi
 done
 
 exit ${exit_code:-0}
